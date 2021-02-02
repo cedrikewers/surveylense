a263dbb647081692752d8aac0edd452ff6a25e66
Florian <Florian@ziegert-bielefeld.de>
2021-02-02 17:05:39 +0100

    created controller, views and edited routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'homepage';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['create'] = 'create';
+$route[''] = 'homepage';
+$route['(:any)'] = 'homepage/$1';
+$route['result/(:any)'] = 'result/$1';
+$route['s/(:any)'] = 'servay/$1';
