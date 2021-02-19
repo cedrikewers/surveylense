2e7acf6229891c742cd6242eb25304f7ae008161
cedrikewers <cedrik.ewers@web.de>
2021-02-19 23:31:01 +0100

    strated adding Create

---
+++ application/views/create/createView.php
@@ -0,0 +1,21 @@
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/Features-Blue.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/Features-Clean.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/Features-Boxed.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/Highlight-Blue.css');?>">
+
+<div style="margin-bottom: 50px">
+    <div class="highlight-blue" sty>
+        <div class="container" style="height: 140px;">
+            <div class="intro">
+                <h2 class="text-center">Create a new Survey<br></h2>
+                <p class="text-center"><br>To create new survey, simply hit the "+" button and add a question<br><br></p>
+            </div>
+        </div>
+        <form style="margin: 0 24px;"><label style="margin: 5px;margin-bottom: 2px;">Question</label><input class="form-control" type="text" style="min-width: 243px;max-width: 70%;margin: 5px;" placeholder="Enter your question here..."><input class="form-control" type="text" style="max-width: 67.3%;margin: 5px;margin-left: 3%;" placeholder="answer option 1..."><button class="btn btn-primary" type="button" style="margin: 5px;margin-left: 3%;"><strong>Add answer option</strong></button></form>
+    </div><button class="btn btn-primary" type="button" style="font-size: x-large;width: 50px;height: 50px;margin-top: -25px;margin-left: 12%;"><strong>+</strong></button>
+</div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
