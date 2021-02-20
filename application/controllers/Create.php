aecb376d7c404144f826f636da1b77f19fef26dd
cedrikewers <cedrik.ewers@web.de>
2021-02-20 22:36:31 +0100

    started database insertion/ error somewhere

--- application/controllers/Create.php
+++ application/controllers/Create.php
@@ -3,10 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Create extends CI_Controller {
 
+    function __construct(){
+        parent::__construct(); 
+        $this->load->model('Create_model');
+        }
+
 	public function index()
 	{
             $this->load->library('template');
             $this->template->set('title', 'Create a new Survey');
             $this->template->load('templates/homepageTemplate','create/createView.php');
 	}
+
+    public function surveyTemp(){//funtioniert nicht
+        $x = true;
+        $randomId ="";
+        while($x){
+            $randomId = substr(uniqid(), 0, 6);
+            if($this->Create_model->check_randomId($randomId)==false){
+                $x = false;
+            }
+        }
+        $name = $_POST['name'];
+        //$user = $_SESSION['id_user'];
+        $this->Create_model->surveyTemp($randomId, $name);
+    }
 }
